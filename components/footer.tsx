7ac05bccdb005763e3610b0b517d8d67630adde5
samuel <2113110283@untels.edu.pe>
2026-01-26 01:28:01 -0500

    Update footer email to digitalgraphic04@gmail.com

--- components/footer.tsx
+++ components/footer.tsx
@@ -52,7 +52,7 @@ export function Footer() {
                   <rect width="20" height="16" x="2" y="4" rx="2" />
                   <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
                 </svg>
-                info@digitalgraphic.com
+                digitalgraphic04@gmail.com
               </li>
               <li className="flex items-center gap-2">
                 <svg
